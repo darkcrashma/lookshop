b9b007cd3cd2aa2b3480f575f35e7f7538cbaecb
Anton Prokopyev <inpokon@gmail.com>
2018-12-16 11:21:08 +0800

    #add header-top menu

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@ var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var concatCSS    = require('gulp-concat-css');
-var ftp          = require('gulp-ftp');
 
 // Static Server + watching sass/html files
 gulp.task('serve', ['sass'], function() {
@@ -12,13 +11,13 @@ gulp.task('serve', ['sass'], function() {
         server: "app/"
     });
 
-    gulp.watch("app/sass/*.sass", ['sass']);
+    gulp.watch("app/sass/**/*.sass", ['sass']);
     gulp.watch("app/*.html").on('change', browserSync.reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    return gulp.src("app/sass/*.sass")
+    return gulp.src("app/sass/**/*.sass")
         .pipe(sass())
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
